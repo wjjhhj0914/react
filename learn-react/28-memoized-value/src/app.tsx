c1726472deaa85675c8d59e616a57f93a43f5774
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-09-02 10:12:16 +0900

    learn. 렌더링 성능 저하 문제 해결 - 계산된 값 메모이제이션 - useMemo 훅 함수 사용 - useEffect 훅의 종속성에 객체 타입을 설정할 경우 주의! - 기본 타입을 설정할 경우, 해당 값이 바뀔 때만 이펙트 실행됨

--- learn-react/28-memoized-value/src/app.tsx
+++ learn-react/28-memoized-value/src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { LearnSection } from '@/components'
 
 export default function App() {
@@ -11,7 +11,7 @@ export default function App() {
 
 // --------------------------------------------------------------------------
 
-const highCostList = Array(5e7)
+const highCostList = Array(1e7)
   .fill(null)
   .map((_, i) => ({ key: i + 1 }))
 
@@ -19,12 +19,33 @@ function MemoizedDemo() {
   const [divideValue, setDivideValue] = useState<number>(1)
 
   console.time('리스트 필터링')
-  const filteredHighCostList = highCostList.filter(({ key }) => {
-    return key % divideValue === 0
-  })
+  // const filteredHighCostList = useMemo(() => 계산된 값, [종속성(반응성데이터)]) // 값을 메모(기억)
+  const filteredHighCostList = useMemo(() => {
+    console.log('계산된 값 메모화')
+    return highCostList.filter(({ key }) => key % divideValue === 0)
+  }, [divideValue])
   console.timeEnd('리스트 필터링')
 
   const [isDark, setIsDark] = useState(true)
+
+  // 비용이 많이드는 연산은 아니지만,
+  // 다시 렌더링될 때, 기억된 값을 사용하도록 useMemo로 캐싱해보세요.
+  // "useMemo 훅은 계산된 값을 기억한다."
+  // 첫 번째 인수로 값을 계산하는 함수를 전달받는다.
+  // 두 번째 인수로 다시 계산을 하도록 하는 반응성 데이터를 설정한다.
+  // useMemo(() => 계산된 값, [반응성데이터1, 반응성데이터2])
+  const memoizedTheme = useMemo(() => {
+    console.log('테마 변경')
+    return isDark
+      ? {
+          backgroundColor: '#000',
+          color: '#fff',
+        }
+      : {
+          backgroundColor: '#fff',
+          color: '#000',
+        }
+  }, [isDark])
 
   const theme = isDark
     ? {
@@ -36,6 +57,23 @@ function MemoizedDemo() {
         color: '#000',
       }
 
+  // 기본 타입이 메모되었는 지 검증
+  useEffect(() => {
+    console.log(
+      `%ctheme.color = ${theme.color}`,
+      'color: maroon; font-weight: 900'
+    )
+  }, [theme.color])
+
+  // 객체 타입이 메모되었는 지 검증
+  useEffect(() => {
+    console.log('%ctheme 변경', 'color: #070787; font-weight: 900')
+  }, [theme])
+
+  useEffect(() => {
+    console.log('memoizedTheme 변경')
+  }, [memoizedTheme])
+
   return (
     <>
       <button
@@ -46,7 +84,7 @@ function MemoizedDemo() {
         테마 스위치
       </button>
 
-      <div className="p-5" style={theme}>
+      <div className="p-5" style={memoizedTheme}>
         <h2 className="text-xl font-semibold mb-6">메모이제이션 훅</h2>
 
         <div role="group">
