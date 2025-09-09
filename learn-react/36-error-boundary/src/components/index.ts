4c22ac7825480a42ca68c8b6a070b38d7d6cc2df
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-09-09 15:04:57 +0900

    Deleted 36-error-boundary

--- learn-react/36-error-boundary/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as LearnSection } from './learn-section'
-export { default as ErrorBoundary } from './error-boundary'
-export { SignUpForm, SignInForm } from './form'
-export { default as Dialog } from './dialog'
