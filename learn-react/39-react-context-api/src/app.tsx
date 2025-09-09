a41593465923b85526431249860c4ffcb056f3a4
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-09-10 08:58:59 +0900

    39번 react-comtext-api

---
+++ learn-react/39-react-context-api/src/app.tsx
@@ -0,0 +1,10 @@
+import { LearnSection } from '@/components'
+import RandomCountUp from './components/random-count-up'
+
+export default function App() {
+  return (
+    <LearnSection title="리액트 컨텍스트를 사용해 데이터 공유">
+      <RandomCountUp />
+    </LearnSection>
+  )
+}
---
+++ learn-react/39-react-context-api/src/utils/index.ts
@@ -0,0 +1,9 @@
+export { default as usePageQuery } from './SPA/use-page-query'
+export { default as navigate } from './SPA/navigate'
+
+export { getTabbableElements, isFocusable } from './tabbable'
+export { default as measureTime } from './measure-time'
+export { default as debounce } from './debounce'
+export { default as throttle } from './throttle'
+export { default as wait } from './wait'
+export { default as tw } from './tw'
