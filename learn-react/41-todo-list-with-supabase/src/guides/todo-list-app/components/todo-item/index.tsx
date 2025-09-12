ebe75263b98392aecf85c5dcaa905f3b70b0004b
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-09-12 13:19:46 +0900

    learn. Supabase x TodoList 연동 4
    - Supabase 데이터 수정 요청 (할 일 완료 여부 / 할 일 내용)

--- learn-react/41-todo-list-with-supabase/src/guides/todo-list-app/components/todo-item/index.tsx
+++ learn-react/41-todo-list-with-supabase/src/guides/todo-list-app/components/todo-item/index.tsx
@@ -10,26 +10,31 @@ import { tw } from '@/utils';
 import { useTodoListDispatch } from '../../context';
 import { type Todo } from '../../reducer';
 import S from './style.module.css';
-import { deleteTodo } from '@/libs/supabase/api/todos';
+import { deleteTodo, updateTodo } from '@/libs/supabase/api/todos';
 import { toast } from 'sonner';
 
 export default function TodoItem({ todo }: { todo: Todo }) {
   const { removeTodo, editTodo } = useTodoListDispatch();
 
-  // 삭제 기능
+  // 할 일 삭제 기능 (비동기 함수로 변경)
   const handleRemove = async () => {
-    const deleteTodoId: Todo['id'] = todo.id;
     // 비동기 처리
-    await deleteTodo(deleteTodoId);
-    console.log(deleteTodoId);
+    const deletedTodo = await deleteTodo(todo.id);
 
     // 동기 처리
-    removeTodo(todo.id);
+    removeTodo(deletedTodo.id);
   };
 
-  // 할 일 완료 여부 토글 기능
-  const handleToggle = (e: ChangeEvent<HTMLInputElement>) => {
-    editTodo({ ...todo, done: e.target.checked });
+  // 할 일 완료 여부 토글 기능 (비동기 함수로 변경)
+  const handleToggle = async (e: ChangeEvent<HTMLInputElement>) => {
+    // 비동기 처리 (Superbase 단에서 처리)
+    const updatedTodo = await updateTodo({
+      id: todo.id,
+      done: e.target.checked,
+    });
+
+    // 동기 처리 (UI 단에서 처리)
+    editTodo(updatedTodo);
   };
 
   const inputRef = useRef<HTMLInputElement>(null);
@@ -106,12 +111,18 @@ function EditMode({
   onEdit: (editTodo: Todo) => void;
   onEditModeOff: () => void;
 }) {
-  const handleEdit = useCallback(() => {
+  // 비동기 함수로 변경
+  const handleEdit = useCallback(async () => {
     const input = ref.current;
 
     if (input) {
-      const editTodo = { ...todo, doit: input.value };
-      onEdit(editTodo);
+      // 비동기 요청 (Supabase 데이터베이스인 Todos 테이블의 행 데이터 업데이트)
+      const updatedTodo = await updateTodo({ id: todo.id, doit: input.value });
+
+      // 동기 요청 (TodoList Context의 상태 업데이트)
+      onEdit(updatedTodo);
+
+      // 편집 모드 종료 (일반 모드로 변경)
       onEditModeOff();
     }
   }, [onEdit, onEditModeOff, todo, ref]);
