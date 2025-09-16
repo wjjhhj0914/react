0a804ff1f5b9843abde45a0478ae9ab3d4bcf6c8
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-09-16 15:51:20 +0900

    learn. use 함수(API) 3
    - use 함수 특징 및 사용법 정리
    - use 함수 사용 시, 주의할 점
    - Promise 인스턴스는 동일한 객체가 전달되어야 함

--- learn-react/44-deferred-value-transition/src/features/use-api/components/users.tsx
+++ learn-react/44-deferred-value-transition/src/features/use-api/components/users.tsx
@@ -1,7 +1,9 @@
 import {
   Suspense,
   use,
+  useEffect,
   useMemo,
+  useState,
   // useEffect,
   // useState
 } from 'react';
@@ -13,35 +15,68 @@ interface User {
   name: string;
 }
 
+// use 함수(API)
+// 특징: 훅이 아님. 그러므로 조건문 또는 반복문 내부에서 사용 가능
+// 용도 1. useContext 훅을 대신할 수 있음 (Context로부터 값을 가져올 수 있음)
+// 용도 2. Promise 인스턴스를 전달받아 Promise의 보류/해결/거절 등 상태에 따른 처리 가능
+//        보류 중일 때는 Suspense의 fallback이 대체 UI로 화면에 렌더링 (로딩)
+//        해결될 때까지 use 함수는 컴포넌트 렌더링을 차단(진행하지 않음) - 동기 코드인 것처럼
+//        해결되면 use 함수는 Promise의 데이터를 반환한다. (컴포넌트 내부에서 사용) - 컴포넌트 렌더링
+//        이전 코드 대비 깔끔하게 코드베이스 유지할 수 있음. (loading, error, data 상태 선언 필요 ❌, 부수 효과 필요 ❌)
+//        거절될 때는 ErrorBoundary의 fallback이 대체 UI로 화면에 렌더링 (오류)
+
 export default function Users() {
   // [실습]
   // use 함수는 Promise를 직접 받아 해결된 값을 반환합니다.
   // Suspense 경계 내에서만 사용할 수 있습니다.
 
   // const [loading, setLoading] = useState<boolean>(false)
+  // const [error, setError] = useState<null | Error>(null)
   // const [data, setData] = useState<null | User[]>(null)
 
   // useEffect(() => {
   //   setLoading(true)
-  //   fetchUsers().then((data) => {
-  //     setData(data)
-  //     setLoading(false)
-  //   })
+  //   setError(null)
+  //
+  //   fetchUsers()
+  //     .then((data) => {
+  //       setData(data)
+  //       setLoading(false)
+  //     })
+  //     .catch((error) => {
+  //       setError(error)
+  //       setLoading(false)
+  //     })
   // }, [])
 
+  // [안전] Promise 인스턴스를 기억하고 있으므로 렌더링과 상관없이 동일한 참조를 하위 컴포넌트에 전달
   const usersPromise = useMemo(() => fetchUsers(), []);
+
+  // [위험] 컴포넌트가 리렌더링 되면 무한 반복에 빠질 수 있다.
+  const [message, setMessage] = useState<string>('');
+
+  useEffect(() => {
+    if (!message) {
+      console.log('Users 렌더링: 메시지 업데이트');
+      setMessage(m => m + '!');
+    }
+  }, [message]);
+
+  // const usersPromise = fetchUsers() // ... Promise 2
 
   return (
     <section className="mt-6 p-6 max-w-xl bg-gray-50 rounded-xl shadow-lg">
       <h3 className="text-xl font-bold mb-6 text-center text-indigo-700">
-        사용자 목록
+        사용자 목록 ({message})
       </h3>
+
       {/* {loading && <Loading />} */}
       {/* {!loading && data && <UserList items={data} />} */}
 
       <ErrorBoundary>
         <Suspense fallback={<Loading />}>
           <UserList fetchUsersPromise={usersPromise} />
+          {/* <UserList /> */}
         </Suspense>
       </ErrorBoundary>
     </section>
@@ -60,6 +95,10 @@ function UserList({
 
   const items = use(fetchUsersPromise);
   console.log(items);
+
+  // 아래처럼 사용하면 무한 루프에 빠짐!! ❌
+  // const usersPromise = fetchUsers()
+  // const items = use(fetchUsers())
 
   // use 함수는 훅 함수가 아니므로,
   // if문 또는 for문 내부에서 사용이 가능!
@@ -107,7 +146,10 @@ function Loading() {
 async function fetchUsers(): Promise<User[]> {
   await wait(1.2);
 
-  throw new Error('데이터 가져오기에 실패했습니다.');
+  // 오류 상황 체크
+  // throw new Error('데이터 가져오기에 실패했습니다.')
+
+  console.log('fetchUsers');
 
   return [
     { id: '1', name: '김민준' },
