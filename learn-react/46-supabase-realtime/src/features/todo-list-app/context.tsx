cf353deef2824bb1ec7a2c03d0886dc96f5480d1
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-09-18 15:43:50 +0900

    learn. Supabase 리얼타임 데이터베이스 1
    - 리얼타임 데이터베이스 변경 감지 이펙트 구성
    - realtimeTodos 유틸리티 함수 작성 (분리 관리)
    - 부수 효과 구독/구독 해제
    - 일반적으로 채팅과 같은 여러 사람이 이용하는 UI 구현에 사용

--- learn-react/46-supabase-realtime/src/features/todo-list-app/context.tsx
+++ learn-react/46-supabase-realtime/src/features/todo-list-app/context.tsx
@@ -5,17 +5,18 @@ import {
   useEffect,
   useMemo,
   useOptimistic,
-} from 'react'
-import { useImmerReducer } from 'use-immer'
-import { useAuth } from '@/contexts/auth'
-import type { Todo } from '@/libs/supabase'
+} from 'react';
+import { useImmerReducer } from 'use-immer';
+import { useAuth } from '@/contexts/auth';
+import { type Todo } from '@/libs/supabase';
 import {
   createTodo,
   deleteTodo,
   readTodos,
+  realtimeTodos,
   updateTodo,
-} from '@/libs/supabase/api/todos'
-import { wait } from '@/utils'
+} from '@/libs/supabase/api/todos';
+import { wait } from '@/utils';
 import {
   ACTION_TYPE,
   type Action,
@@ -33,24 +34,24 @@ import {
   setTodoListStorageData,
   setTodosAction,
   toggleDoneAction,
-} from './reducer'
+} from './reducer';
 
 interface TodoListDispatchContextValueValue {
   // 서버 측 데이터(server data) 상태
-  addTodo: (newTodo: Todo) => void
-  removeTodo: (removeId: Todo['id']) => void
-  editTodo: (editTodo: Todo) => void
+  addTodo: (newTodo: Todo) => void;
+  removeTodo: (removeId: Todo['id']) => void;
+  editTodo: (editTodo: Todo) => void;
   // 클라이언트 측 데이터(client data) 상태
-  searchTodos: (search: State['search']) => void
-  toggleDone: () => void
+  searchTodos: (search: State['search']) => void;
+  toggleDone: () => void;
 }
 
-const TodoListContext = createContext<State | null>(null)
-TodoListContext.displayName = 'TodoListContext'
+const TodoListContext = createContext<State | null>(null);
+TodoListContext.displayName = 'TodoListContext';
 
 const TodoListDispatchContext =
-  createContext<TodoListDispatchContextValueValue | null>(null)
-TodoListDispatchContext.displayName = 'TodoListDispatchContext'
+  createContext<TodoListDispatchContextValueValue | null>(null);
+TodoListDispatchContext.displayName = 'TodoListDispatchContext';
 
 export default function TodoListProvider({
   children,
@@ -63,95 +64,125 @@ export default function TodoListProvider({
     reducer,
     initialState,
     init
-  )
+  );
 
+  // --------------------------------------------------------------------------
   // 낙관적인 업데이트 상태
   const [optimisticTodos, updateOptimisticTodos] = useOptimistic(
     state.todos,
     (todos: State['todos'], action: Action) => {
       switch (action.type) {
         case ACTION_TYPE.ADD_TODO: {
-          const { newTodo } = action.payload
-          return [newTodo, ...todos]
+          const { newTodo } = action.payload;
+          return [newTodo, ...todos];
         }
         case ACTION_TYPE.REMOVE_TODO: {
-          const { removeId } = action.payload
-          return todos.filter((todo) => todo.id !== removeId)
+          const { removeId } = action.payload;
+          return todos.filter(todo => todo.id !== removeId);
         }
         case ACTION_TYPE.EDIT_TODO: {
-          const { editTodo } = action.payload
-          return todos.map((todo) =>
-            todo.id === editTodo.id ? editTodo : todo
-          )
+          const { editTodo } = action.payload;
+          return todos.map(todo => (todo.id === editTodo.id ? editTodo : todo));
         }
         case ACTION_TYPE.SEARCH_TODOS:
         default: {
-          return todos
+          return todos;
         }
       }
     }
-  )
+  );
 
+  // --------------------------------------------------------------------------
   // 액션 함수
   const actions = useMemo(
     () => ({
       addTodo: async (newTodo: Todo) => {
-        updateOptimisticTodos(addTodoAction(newTodo))
+        updateOptimisticTodos(addTodoAction(newTodo));
         try {
-          await wait(1.2, { forceResolved: true })
-          const createdTodo = await createTodo({ doit: newTodo.doit })
-          dispatch(addTodoAction(createdTodo))
+          await wait(1.2, { forceResolved: false });
+          const createdTodo = await createTodo({ doit: newTodo.doit });
+          dispatch(addTodoAction(createdTodo));
         } catch {
-          updateOptimisticTodos(optimisticRollbackAction())
+          updateOptimisticTodos(optimisticRollbackAction());
         }
       },
       removeTodo: async (removeId: Todo['id']) => {
-        const action = removeTodoAction(removeId)
-        updateOptimisticTodos(action)
+        const action = removeTodoAction(removeId);
+        updateOptimisticTodos(action);
         try {
-          await wait()
-          await deleteTodo(removeId)
-          dispatch(removeTodoAction(removeId))
+          await wait();
+          await deleteTodo(removeId);
+          dispatch(removeTodoAction(removeId));
         } catch {
-          updateOptimisticTodos(optimisticRollbackAction())
+          updateOptimisticTodos(optimisticRollbackAction());
         }
       },
       editTodo: async (editTodo: Todo) => {
-        updateOptimisticTodos(editTodoAction(editTodo))
+        updateOptimisticTodos(editTodoAction(editTodo));
         try {
-          await wait()
-          const updatedTodo = await updateTodo(editTodo)
-          dispatch(editTodoAction(updatedTodo))
+          await wait();
+          const updatedTodo = await updateTodo(editTodo);
+          dispatch(editTodoAction(updatedTodo));
         } catch {
-          updateOptimisticTodos(optimisticRollbackAction())
+          updateOptimisticTodos(optimisticRollbackAction());
         }
       },
       searchTodos: (search: State['search']) => {
-        dispatch(searchTodosAction(search))
+        dispatch(searchTodosAction(search));
       },
       toggleDone: () => {
-        dispatch(toggleDoneAction())
+        dispatch(toggleDoneAction());
       },
     }),
     [dispatch, updateOptimisticTodos]
-  )
+  );
+
+  // --------------------------------------------------------------------------
+  // 리얼타임 데이터베이스 변경 감지 이펙트
+  useEffect(() => {
+    // 구독 해제 함수 <- public.todos 테이블 변경 감지 구독
+    const unsubscribe = realtimeTodos(payload => {
+      const { eventType, new: _newData } = payload;
+
+      switch (eventType) {
+        case 'INSERT': {
+          console.log('추가');
+          break;
+        }
+        case 'UPDATE': {
+          console.log('수정');
+          break;
+        }
+        case 'DELETE': {
+          console.log('삭제');
+          break;
+        }
+      }
+    });
+
+    // 정리(cleanup)
+    return () => {
+      // 구독 중인 데이터베이스 테이블을 구독 해제
+      unsubscribe();
+    };
+  }, []);
 
   // --------------------------------------------------------------------------
   // 인증(Auth)
 
-  const { isAuthenticated } = useAuth()
+  const { isAuthenticated } = useAuth();
 
   useEffect(() => {
     if (isAuthenticated) {
       // 로그인 시, todos 데이터 가져오기
-      readTodos().then((todos) => {
-        dispatch(setTodosAction(todos))
-      })
+      readTodos().then(todos => {
+        dispatch(setTodosAction(todos));
+      });
     } else {
       // 로그아웃 시, todos 데이터 초기화
-      dispatch(setTodosAction([]))
+      dispatch(setTodosAction([]));
     }
-  }, [dispatch, isAuthenticated])
+  }, [dispatch, isAuthenticated]);
 
   // --------------------------------------------------------------------------
   // 유지(Persist)
@@ -159,12 +190,12 @@ export default function TodoListProvider({
   useEffect(() => {
     if (persist) {
       // 웹 스토리지에 데이터 저장
-      setTodoListStorageData(state)
+      setTodoListStorageData(state);
     } else {
       // 웹 스토리지에서 데이터 삭제
-      removeTodoListStorageData()
+      removeTodoListStorageData();
     }
-  }, [state, persist])
+  }, [state, persist]);
 
   return (
     <TodoListDispatchContext value={actions}>
@@ -172,30 +203,30 @@ export default function TodoListProvider({
         {children}
       </TodoListContext>
     </TodoListDispatchContext>
-  )
+  );
 }
 
 /* eslint-disable react-refresh/only-export-components */
 export const useTodoList = () => {
-  const state = useContext(TodoListContext)
+  const state = useContext(TodoListContext);
 
   if (!state) {
     throw new Error(
       'useTodoList 훅은 TodoListProvider 내부에서만 사용 가능합니다.'
-    )
+    );
   }
 
-  return state
-}
+  return state;
+};
 
 export const useTodoListDispatch = () => {
-  const actions = useContext(TodoListDispatchContext)
+  const actions = useContext(TodoListDispatchContext);
 
   if (!actions) {
     throw new Error(
       'useTodoListDispatch 훅은 TodoListProvider 내부에서만 사용 가능합니다.'
-    )
+    );
   }
 
-  return actions
-}
+  return actions;
+};
--- learn-react/46-supabase-realtime/src/libs/supabase/api/todos.ts
+++ learn-react/46-supabase-realtime/src/libs/supabase/api/todos.ts
@@ -1,16 +1,54 @@
 // --------------------------------------------------------------------------
 // ✅ Supabase의 Todos 테이블용 클라이언트 API(함수)
 // --------------------------------------------------------------------------
-import { toast } from 'sonner'
-import supabase, { type Todo, type TodoInsert, TodoUpdate } from '../index'
-import requiredUser from './required-user'
+import { type RealtimePostgresChangesPayload } from '@supabase/supabase-js';
+import { toast } from 'sonner';
+import supabase, { type Todo, type TodoInsert, TodoUpdate } from '../index';
+import requiredUser from './required-user';
+
+// --------------------------------------------------------------------------
+// 리억타임(Realtime)
+
+export const realtimeTodos = (
+  callback: (
+    payload: RealtimePostgresChangesPayload<{ [key: string]: unknown }>
+  ) => void
+) => {
+  // 리얼타임 데이터베이스의 테이블을 구독(서브스크립션)
+  // 채널 생성
+  const todosChannel = supabase.channel('public.todos');
+
+  // 생성된 채널의 이벤트 설정
+  todosChannel
+    .on(
+      'postgres_changes',
+      { event: '*', schema: 'public', table: 'todos' },
+      payload => {
+        // console.log('실시간 데이터베이스 변경 이벤트', payload.eventType)
+        // console.log('변경된 새로운 데이터', payload.new)
+        // console.log('변경 이전 데이터', payload.old)
+        callback(payload);
+      }
+    )
+    .subscribe(status => {
+      console.log('실시간 todos 데이터베이스 구독 상태:', status);
+    });
+
+  // 구독 중인 데이터베이스 테이블을 구독 해제 함수
+  const unsubscribe = async () => {
+    const result = await supabase.removeChannel(todosChannel);
+    console.log('실시간 todos 데이터베이스 구독 해제 상태:', result);
+  };
+
+  return unsubscribe;
+};
 
 // --------------------------------------------------------------------------
 // 생성(Create)
 
 export const createTodo = async (newTodo: TodoInsert): Promise<Todo> => {
   // 서버에 요청하기 전에 인증된 사용자인지 검증!
-  const user = await requiredUser()
+  const user = await requiredUser();
 
   const { error, data: createdTodo } = await supabase
     .from('todos')
@@ -19,37 +57,37 @@ export const createTodo = async (newTodo: TodoInsert): Promise<Todo> => {
       { ...newTodo, user_id: user.id },
     ])
     .select('*')
-    .single()
+    .single();
 
   if (error) {
-    const errorMessage = '할 일(Todo) 생성 실패!'
-    toast.error(`${errorMessage} ${error.message}`)
-    throw new Error(errorMessage)
+    const errorMessage = '할 일(Todo) 생성 실패!';
+    toast.error(`${errorMessage} ${error.message}`);
+    throw new Error(errorMessage);
   }
 
-  return createdTodo
-}
+  return createdTodo;
+};
 
 // --------------------------------------------------------------------------
 // 조회(Read)
 
 export const readTodos = async (): Promise<Todo[]> => {
-  const user = await requiredUser()
+  const user = await requiredUser();
 
   const { error, data: todos } = await supabase
     .from('todos')
     .select('*')
     .eq('user_id', user.id)
-    .order('created_at', { ascending: false })
+    .order('created_at', { ascending: false });
 
   if (error) {
-    const errorMessage = '할 일 목록(Todos) 조회 실패!'
-    toast.error(`${errorMessage} ${error.message}`)
-    throw new Error(errorMessage)
+    const errorMessage = '할 일 목록(Todos) 조회 실패!';
+    toast.error(`${errorMessage} ${error.message}`);
+    throw new Error(errorMessage);
   }
 
-  return todos ?? []
-}
+  return todos ?? [];
+};
 
 // --------------------------------------------------------------------------
 // 수정(Update)
@@ -57,7 +95,7 @@ export const readTodos = async (): Promise<Todo[]> => {
 export const updateTodo = async (
   updateTodo: Omit<TodoUpdate, 'id' | 'created_at'> & { id: Todo['id'] }
 ): Promise<Todo> => {
-  const user = await requiredUser()
+  const user = await requiredUser();
 
   const { error, data: updatedTodo } = await supabase
     .from('todos')
@@ -65,22 +103,22 @@ export const updateTodo = async (
     .eq('id', updateTodo.id)
     .eq('user_id', user.id)
     .select('*')
-    .single()
+    .single();
 
   if (error) {
-    const errorMessage = '할 일(Todo) 수정 실패!'
-    toast.error(`${errorMessage} ${error.message}`)
-    throw new Error(errorMessage)
+    const errorMessage = '할 일(Todo) 수정 실패!';
+    toast.error(`${errorMessage} ${error.message}`);
+    throw new Error(errorMessage);
   }
 
-  return updatedTodo
-}
+  return updatedTodo;
+};
 
 // --------------------------------------------------------------------------
 // 삭제(Delete)
 
 export const deleteTodo = async (deleteTodoId: Todo['id']): Promise<Todo> => {
-  const user = await requiredUser()
+  const user = await requiredUser();
 
   const { error, data: deletedTodo } = await supabase
     .from('todos')
@@ -88,13 +126,13 @@ export const deleteTodo = async (deleteTodoId: Todo['id']): Promise<Todo> => {
     .eq('id', deleteTodoId)
     .eq('user_id', user.id)
     .select()
-    .single()
+    .single();
 
   if (error) {
-    const errorMessage = '할 일(Todo) 삭제 실패!'
-    toast.error(`${errorMessage} ${error.message}`)
-    throw new Error(errorMessage)
+    const errorMessage = '할 일(Todo) 삭제 실패!';
+    toast.error(`${errorMessage} ${error.message}`);
+    throw new Error(errorMessage);
   }
 
-  return deletedTodo
-}
+  return deletedTodo;
+};
