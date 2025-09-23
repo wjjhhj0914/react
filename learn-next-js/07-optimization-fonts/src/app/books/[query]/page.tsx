e79b2777301f6cebbf8149d7cfb5bc014a50ffde
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-09-23 15:44:57 +0900

    learn. Next.js의 폰트 최적화 3 & 4
    (3)
    - 로컬 폰트 최적화
    - Pretendard Variable
    
    (4)
    - 로컬 폰트 최적화
    - Spoqa Han Sans Neo
    - 비가변 폰트

--- learn-next-js/07-optimization-fonts/src/app/books/[query]/page.tsx
+++ learn-next-js/07-optimization-fonts/src/app/books/[query]/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Section } from '@/components'
+import { spoqaHandSansNeo } from '@/fonts'
 import { wait } from '@/utils'
 import fetchBooks from '../api'
 import Books from '../components/books'
@@ -22,10 +23,10 @@ export default async function BooksQueryResultPage({ params }: Props) {
   const booksData = await fetchBooks({ query: decodedQuery })
 
   const title = (
-    <>
+    <span className={spoqaHandSansNeo.className}>
       <strong>{decodedQuery}</strong>
       검색 결과
-    </>
+    </span>
   )
 
   return (
--- learn-next-js/07-optimization-fonts/src/fonts/index.ts
+++ learn-next-js/07-optimization-fonts/src/fonts/index.ts
@@ -1,19 +1,74 @@
 import { Noto_Sans_KR, Gothic_A1 } from 'next/font/google'
+import localFont from 'next/font/local'
 
+// --------------------------------------------------------------------------
+// Google Fonts
+
+// Noto Sans KR
 export const notoSansKR = Noto_Sans_KR({
   subsets: ['latin'],
   variable: '--noto-sans-kr',
 })
 
+// Gothid A1
 export const gothicA1 = Gothic_A1({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   variable: '--gothic-a1',
 })
 
+// --------------------------------------------------------------------------
+// Local Fonts
+
+// Pretendard Variable
+
+export const pretendard = localFont({
+  variable: '--pretendard',
+  src: './PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+})
+
+// Spoka Han Sans
+
+export const spoqaHandSansNeo = localFont({
+  variable: '--spoqa-han-sans-neo',
+  src: [
+    {
+      path: './SpoqaHanSansNeo-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './SpoqaHanSansNeo-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './SpoqaHanSansNeo-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './SpoqaHanSansNeo-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './SpoqaHanSansNeo-Thin.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+  ],
+})
+
+// --------------------------------------------------------------------------
+// Fonts Module
+
 const fonts = {
   notoSansKR,
   gothicA1,
+  pretendard,
 }
 
 export default fonts
